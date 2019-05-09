c4978e7cf1ad529b3a1f762ec9dde2926655f81e
take8 <37664118+take8@users.noreply.github.com>
2019-05-10 01:26:23 +0900

    reverseパイプの実装

--- src/app/pipes/reverse.pipe.ts
+++ src/app/pipes/reverse.pipe.ts
@@ -6,7 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ReversePipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    return null;
+    if (!value) {
+      return;
+    }
+    return value.reverse();
   }
 
 }
