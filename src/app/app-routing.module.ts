49bc2098b7278cfc6ab62df65f309255b989b3a1
take8 <37664118+take8@users.noreply.github.com>
2019-05-09 23:54:16 +0900

    チャネル詳細のルーティングを追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChannelComponent } from './channel/channel.component';
 
-const routes: Routes = [];
+// Routes にルーティング情報を定義する
+const routes: Routes = [
+  {
+    path: 'channels/:channelName',
+    component: ChannelComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/channel/channel.component.ts
+++ src/app/channel/channel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 /**
  * チャネル詳細コンポーネント
@@ -9,10 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./channel.component.css']
 })
 export class ChannelComponent implements OnInit {
+  public name: string;
 
-  constructor() { }
+  // DIが働いている
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // this.route.params が Observable を返す(RxJS の interface)
+    this.route.params.subscribe(res => {
+      this.name = res['channelName'];
+    });
   }
 
 }
