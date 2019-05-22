749a34723b9d75fc050f6dbcf88128510669ed84
take8 <37664118+take8@users.noreply.github.com>
2019-05-23 01:40:48 +0900

    Angular Materialのサンプルに沿うように修正

--- src/app/services/channel.service.ts
+++ src/app/services/channel.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 import { Observable } from 'rxjs';
+
 import { Channel } from '../shared/models/channel';
 import { AppConst } from '../app-const';
 
